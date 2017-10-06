1adef2c5cbdc75a38ded1b535a1bcfc789085896
Andrey <595999+turova@users.noreply.github.com>
2017-10-05 20:02:13 -0400

    Updated sources in run.sh

--- run.sh
+++ run.sh
@@ -6,6 +6,7 @@ git checkout 1.17.0
 
 # Add extension gallery support
 # Borrowed from https://github.com/Microsoft/vscode/issues/23831
+# and https://github.com/Microsoft/vscode/issues/31168
 sed -i "s/^}/\t,\"extensionsGallery\": {\n\t\t\"serviceUrl\": \"https:\/\/marketplace.visualstudio.com\/_apis\/public\/gallery\",\n\t\t\"cacheUrl\": \"https:\/\/vscode.blob.core.windows.net\/gallery\/index\",\n\t\t\"itemrl\": \"https:\/\/marketplace.visualstudio.com\/items\"\n\t}\n}/g" product.json
 
 # unsafe-perm flag necessary when installing dependencies as root
